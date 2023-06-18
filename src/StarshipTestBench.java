4738e5cc0c5a1b406f798263d2b416b50b377c2d
MiskiKei <107701715+MiskiKei@users.noreply.github.com>
2023-06-18 11:38:08 -0400

    Implemented test-benches for validating class methods

--- src/StarshipTestBench.java
+++ src/StarshipTestBench.java
@@ -2,21 +2,37 @@
 public class StarshipTestBench {
 
 	public static void main(String[] args) {
-		
+
 		System.out.print("Starting the testbench for class Starship");
-		
-		Starship ship1 = new Starship ("gold", 1998, 85.0, 3456);
-		
-		double result = ship1.getRemFuel();
-		
-		/*if (result == 85) {
-			System.out.println("looks like your function is working");
-		}else {
-			System.out.println("Problem with getRemFuel");
-		} */
-		
+
+		Starship starship = new Starship("gold", 1998, 85.0, 3456);
+
+		double result = starship.getRemFuel();
+
 		if (result != 85) {
 			System.out.println("Problem with getRemFuel");
 		}
-	}
+
+		starship.setAutoPilot(true);
+
+		boolean result1 = starship.isAutoPilot();
+
+		if (!result1) {
+			System.out.println("Problem with setAutoPilot");
+		}
+        starship.calculateDistanceToBeTraveled(100, 200);
+
+        double result2 = starship.getTotalDistanceTraveled();
+
+        if (result2 != 100.0) {
+            System.out.println("Problem with calculateDistanceToBeTraveled");
+        }
+        starship.setVelocity(50); // Assuming velocity is 50 m/s
+
+        double result3 = starship.calculateETA();
+
+        if (result3 != 2.0) {
+            System.out.println("Problem with calculateETA");
+        }
+    }
 }
